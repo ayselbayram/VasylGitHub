1634d5a9c5654cd4e8739b9dc8f8b2a08143c8e3
Aysel Bayram <ayselbyram83@gmail.com>
2019-12-05 15:25:20 -0500

    removed colors

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -2,9 +2,8 @@ package java_code;
 
 public class Person {
    private int age;
-    private String firstName;
 
-    private int SSN;
-   private String Address;
-
+public Person(int age){
+    this.age=age;
+}
 }
